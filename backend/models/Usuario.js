47d12f32fdc5a591e65f59b5351afac8b0daa376
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2024-09-18 15:34:57 -0500

    BACKEND-DMS - Definiendo los modelos para conexión con BD PostgreSQL

---
+++ backend/models/Usuario.js
@@ -0,0 +1,27 @@
+import pg from "pg"; // Importar el cliente de PostgreSQL
+
+const usuarioSchema = async () => {
+  const query = `
+     CREATE TABLE IF NOT EXISTS usuarios (
+      id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- Identificador único autogenerado
+      nombres VARCHAR(100) NOT NULL,
+      apellidos VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL, -- Email único y requerido
+      password VARCHAR(150) NOT NULL, -- Contraseña encriptada
+      rol VARCHAR(50) CHECK ( rol IN ( 'admin', 'usuario' ) ) DEFAULT 'usuario', -- Rol del usuario (admin o usuario)
+      token VARCHAR(100) DEFAULT 'null',
+      confirmado BOOLEAN DEFAULT FALSE,
+      fecha_creación TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      fecha_actualización TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+ `;
+
+  try {
+    await conectarDB.query(query);
+    console.log("Tabla 'usuarios' creada correctamente");
+  } catch (error) {
+    console.error("Error creando la tabla 'usuarios':", error);
+  }
+};
+
+usuarioSchema();
