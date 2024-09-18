3d3d7188cb03bdea238f5dee91f0c657c9b66952
Jasmany Franco <jasmanyfranco@gmail.com>
2024-09-18 05:22:29 -0500

    BACKEND-DMS - Se instaló la dependencia del cliente de PostgreSQL, se creo el archivo de configuración de la BD y se lo importo en el index.js principal para arrancar mi servidor Node.js con Express

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 dotenv.config(); // Escanea y busca el archivo .env
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+conectarDB();
 
 // Routing
 app.use("/", (req, res) => {
