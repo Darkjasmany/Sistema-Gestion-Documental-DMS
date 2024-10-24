730c8f824cfc805e15b248589c146ec72fada607
Jasmany Franco <jasmanyfranco@gmail.com>
2024-10-24 05:30:00 -0500

    BACKEND-DMS - validarFecha.js -> se actualizo para que considere el registro de fecha de hoy y posteriores

--- backend/src/utils/validarFecha.js
+++ backend/src/utils/validarFecha.js
@@ -1,20 +1,18 @@
 export const validarFecha = (fechaEnviada) => {
   // Obtenemos la fecha de hoy y la reseteamos a las 00:00:00
   const hoy = new Date();
-  hoy.setHours(0, 0, 0, 0);
+  hoy.setUTCHours(0, 0, 0, 0); // Forzamos la fecha de hoy a medianoche UTC
 
   // Convertimos la fecha enviada a un objeto Date
   const fecha = new Date(fechaEnviada);
 
   // Si la fecha no es válida
-  if (isNaN(fecha.getTime())) {
+  if (isNaN(fecha.getTime()))
     return { valido: false, mensaje: "Fecha inválida" };
-  }
 
   // Si la fecha enviada es anterior a hoy
-  if (fecha < hoy) {
+  if (fecha < hoy)
     return { valido: false, mensaje: "La fecha no puede ser anterior a hoy" };
-  }
 
   // Si la fecha es válida
   return { valido: true };
