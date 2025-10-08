6b81ca89011333eee9083d377fc9b00740d985f0
Jasmany Franco <jasmanyfranco@gmail.com>
2025-10-08 06:37:39 -0500

    BACKEND: Separando los Types(tipo) del Schema de Zod que tienen las validaciónes. FRONTEND: Actualizando dependencias y configurando TailwindCSS

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   // base: "/dms/", // Reemplaza 'mi-app' con el nombre de tu subdirectorio
 });
