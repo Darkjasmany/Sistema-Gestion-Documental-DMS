de503d98140db560cf3ac0261dc4b63597a3bf97
Jasmany Franco Trabajo <vicente.franco@naranjal.gob.ec>
2025-11-05 12:46:29 -0500

    SELNIC - Configuracion de Monorepo y Tuborepo, Solventado el import del @selnic/shared , modificando el tsconfig.json del backend para que cubra las entradas de ese packages/shared apunando a la raiz del proyecto sin compilarlo en desarrollo

--- packages/shared/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./models";
-export * from "./schemas";
-//# sourceMappingURL=index.d.ts.map
--- packages/shared/src/index.js
+++
@@ -1,6 +0,0 @@
-// Exportar TODOS los schemas y types
-// Models
-export * from "./models";
-// Schemas generales
-export * from "./schemas";
-//# sourceMappingURL=index.js.map
