31e4f05a57c0eef159fd78c069814389993e377b
Jasmany Franco <jasmanyfranco@gmail.com>
2025-11-17 06:30:47 -0500

    SELNIC - FRONTEND: NewPasswordToken.pages -> Interfaz para escribir el token

--- apps/frontend/src/components/auth/NewPasswordToken.pages.tsx
+++ apps/frontend/src/components/auth/NewPasswordToken.pages.tsx
@@ -1,5 +1,8 @@
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
 import type { TokenInput } from "@selnic/shared";
 import type React from "react";
+
+import { Link } from "react-router-dom";
 
 interface NewPasswordTokenProps {
   token: TokenInput;
@@ -8,7 +11,32 @@ interface NewPasswordTokenProps {
 }
 
 const NewPasswordToken = ({ token, setToken, setIsValidToken }: NewPasswordTokenProps) => {
-  return <div>NewPasswordToken</div>;
+  const handleChange = () => {};
+
+  const handleComplete = () => {};
+
+  return (
+    <>
+      <form className="space-y-8 p-10 rounded-lg bg-white mt-10">
+        <label className="font-normal text-2xl text-center block">Código de 6 dígitos</label>
+        <div className="flex justify-center gap-5">
+          <PinInput value={token} onChange={handleChange} onComplete={handleComplete}>
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+            <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
+          </PinInput>
+        </div>
+      </form>
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link to="/auth/forgot-password" className="text-center text-gray-300 font-normal">
+          Solicitar un nuevo Código
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default NewPasswordToken;
